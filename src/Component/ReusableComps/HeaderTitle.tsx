dec66faa2c0dd542f4e43fd8c77a74a0cf260d58
smile995 <ah.smile995@gmail.com>
2025-09-11 10:58:25 +0600

    Added about me content with animation

--- src/Component/ReusableComps/HeaderTitle.tsx
+++ src/Component/ReusableComps/HeaderTitle.tsx
@@ -1,3 +1,5 @@
+
+import "aos/dist/aos.css";
 const HeaderTitle = ({
   
   title,
@@ -7,7 +9,7 @@ const HeaderTitle = ({
   subTitle: string;
 }) => {
   return (
-    <div className="md:w-1/2 lg:w-1/3  rounded-full w-full mx-auto border-b-4 border-gray-200 border-dashed mb-5 md:mb-10 ">
+    <div data-aos="fade-up" data-aos-duration="1200" className="md:w-1/2 lg:w-1/3  rounded-full w-full mx-auto border-b-4 border-gray-200 border-dashed mb-3 md:mb-6 ">
       <h1 className="md:text-4xl text-2xl font-bold text-center mb-2.5">
         <span className="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text">
           {title}
--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -3,60 +3,89 @@ import EducationTimeline from "../Component/ReusableComps/EducationTimeline";
 import "aos/dist/aos.css";
 import HeaderTitle from "../Component/ReusableComps/HeaderTitle";
 import Lottie from "lottie-react";
-import education from "../assets/Lotties/education.json"
+import education from "../assets/Lotties/education.json";
+import hello from "../assets/Lotties/hello.json";
 const AboutPage = () => {
   return (
     <Container>
-      <section>
-        {/* page heading */}
+      <section  >
         <HeaderTitle
-          title="Know About Myself"
+          title="Introducing Myself"
           subTitle="Explore about myself with valuable informations"
         />
       </section>
-      <section>
-        {/* Intro about me */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 itece">
-          <div>Professional Image</div>
-          <div>
-            {/* summary and objective */}
-            <p className="text-lg text-gray-700 mb-6 ">
-              Hi, I’m{" "}
-              <span className="font-semibold text-primary">
-                Amir Hamza Ismail
-              </span>
-              , a passionate Full Stack Web Developer from Bangladesh with a
-              focus on building user-friendly and performance-driven web
-              applications. I love turning complex problems into elegant,
-              scalable solutions.
-            </p>
+      <section data-aos="fade-up" data-aos-duration="1200" >
+        <div className="grid grid-cols-1  md:grid-cols-5 gap-3 ">
+          <div className="p-5 md:p-8 col-span-2 ">
+            <Lottie className="w-full" animationData={hello} loop={true} />
+          </div>
+          <div className="px-5 md:px-8 space-y-3 col-span-3">
+            <div>
+              <h2 className="text-2xl font-bold text-primary mb-2">About Me</h2>
+              <p className="text-lg text-gray-700">
+                I’m a passionate{" "}
+                <span className="font-semibold">Full Stack Web Developer</span>{" "}
+                skilled in React.js, Next.js, Node.js, Express.js, and MongoDB.
+                I focus on building scalable, user-friendly applications with
+                clean code and optimized performance. My goal is to leverage my
+                MERN stack expertise and problem-solving mindset to create
+                innovative solutions while continuously learning and growing as
+                a developer.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-2xl font-bold text-primary ">
+                Currently Learning
+              </h2>
+              <ul className="list-disc list-inside text-lg text-gray-700 space-y-1">
+                <li>
+                  Advanced Next.js features (App Router, parallel routes,
+                  intercepting routes)
+                </li>
+                <li>Machine Learning basics (from workshops & self-study)</li>
+                <li>Cloud deployment and optimization for production apps</li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="text-2xl font-bold text-primary ">
+                Hobbies and Interests{" "}
+              </h2>
+              <ul className="list-disc grid lg:grid-cols-2 list-inside text-lg text-gray-700 space-y-1">
+                <li>Coding & competitions</li>
+                <li>Exploring new tech</li>
+                <li>Robotics & hardware</li>
+                <li>Scalable web apps</li>
+                <li>Problem-solving</li>
+                <li>Open-source work</li>
+                <li>AI & automation</li>
+                <li>Machine & Deep Learning</li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
 
       {/* Education & Background */}
-      <div className="mb-6">
-        <EducationTimeline />
-      </div>
+      <section data-aos="fade-up" data-aos-duration="1200">
+        <div>
+          <EducationTimeline />
+        </div>
+      </section>
 
       {/* Skills Summary */}
-      <section>
+      <section data-aos="fade-up" data-aos-duration="1200" className="bg-yellow-200 ">
         <div>skills with animation and transection</div>
       </section>
 
       {/* Experience */}
-      <section>
+      <section data-aos="fade-up" data-aos-duration="1200"  className="bg-purple-300 ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3 items-center">
           <div>
-            <Lottie animationData={education} loop={true}  />
+            <Lottie animationData={education} loop={true} />
           </div>
           <div>Datails of experience</div>
         </div>
       </section>
-
-      {/* Mission or Goal */}
-
-      {/* Fun Facts / Hobbies */}
     </Container>
   );
 };
